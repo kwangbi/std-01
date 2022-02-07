e1309a823577929a0694d8f160655b62b79738f7
kwangbi <kwangbi@naver.com>
2022-02-08 08:58:19 +0900

    commit

--- pages/signin.js
+++ pages/signin.js
@@ -1,22 +1,83 @@
 import { useState } from "react";
 import InputGroupComp from "../components/form/inputGroup";
 import ButtonComp from "../components/form/button";
+import axios from "axios";
+import Router from "next/router";
+
 
 const SignInPage = () => {
     const [userId, setUserId] = useState('');
     const [userPwd, setUserPwd] = useState('');
+    const [userInfo, setUserInfo] = useState({
+        userId: "",
+        userPwd: ""
+    })
 
     const handleChangeInput = (name, value) => {
-        /*         console.log("name : " + name);
-                console.log("value : " + value);
-         */
         name === 'userId' ? setUserId(value) : name === 'userPwd' ? setUserPwd(value) : null
+    }
 
+    const handleChange = (evt) => {
+        const { name, value } = evt.target;
+        setUserInfo({
+            ...userInfo,
+            [name]: value
+        })
     }
 
     const handleSubmit = () => {
         console.log("userId : " + userId);
         console.log("userPwd : " + userPwd);
+        if (!userId) { return alert('아이디를 입력하세요') }
+        if (!userPwd) { return alert('비밀번호를 입력하세요') }
+
+        // fetch('apiURL', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Accept': '*/*',
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(userId)
+        // })
+        //     .then(res => {
+        //         console.log(res)
+        //         return res.json();
+        //     }).then(responseJson => {
+        //         console.log(responseJson)
+        //         //const result = responseJson.data.result;
+        //     }).catch(err => {
+        //         console.log('err : ' + err)
+        //     })
+
+        axios({
+            url: "/signin",
+            method: "post",
+            data: {
+                user_id: userId,
+                user_pwd: userPwd
+            }
+        }).then(res => {
+            console.log(res.data);
+            // console.log(res.data)
+            // const result = res.data.result;
+            // localStorage.setItem('userInfo', JSON.stringify(result))
+            // Router.push('/main')
+            localStorage.setItem('userInfo', JSON.stringify(res.data));
+            const storageData = JSON.parse(localStorage.getItem('userInfo'));
+            console.log('storageData', storageData);
+            Router.push('/main');
+        }).catch(err => {
+            console.log(err);
+            // console.log(err.response.status)
+            // const status = err.response.status;
+            // if (status === 401) {
+            //     return alert('비밀번호가 유효하지 않습니다.');
+            // }
+            // if (status === 404) {
+            //     return alert('페이지를 찾을 수 없습니다.');
+            // }
+        })
+
     }
 
 
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const next = require('next');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const dotenv = require('dotenv');
+
+const dev = process.env.NODE_ENV !== 'production';
+const prod = process.env.NODE_ENV === 'production';
+
+dotenv.config();
+
+const app = next({ dev }); // next 모듈을 사용
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = express(); // back 서버에서의 const app = express()
+
+    server.use(morgan('dev'));
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
+    //server.use(cookieParser(process.env.COOKIE_SECRET));
+    server.use(cookieParser("!SDFDESSRTGHGFDGFDS!@#SdDD345"));
+    server.use(
+        expressSession({
+            resave: false,
+            saveUninitialized: false,
+            //secret: process.env.COOKIE_SECRET, // backend 서버와 같은 키를 써야한다.
+            secret: "!SDFDESSRTGHGFDGFDS!@#SdDD345", // backend 서버와 같은 키를 써야한다.
+            cookie: {
+                httpOnly: true,
+                secure: false,
+            },
+        }),
+    );
+
+    server.post('/signin', (req, res) => {
+        console.log(req.body);
+        res.json({ 'userId': 1 });
+    })
+
+    // server.get('/hashtag/:tag', (req, res) => {
+    //     return app.render(req, res, '/hashtag', { tag: req.params.tag });
+    // });
+
+    // server.get('/user/:id', (req, res) => {
+    //     return app.render(req, res, '/user', { id: req.params.id });
+    // });
+
+    server.get('*', (req, res) => { // 모든 get 요청 처리
+        return handle(req, res); // next의 get 요청 처리기
+    });
+
+    server.listen(3000, () => {
+        console.log('next+expresss running on port 3000');
+    });
+});
